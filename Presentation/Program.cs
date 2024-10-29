83b4545a0aa8d89ef640922065c37e55b65535f0
unknown <Administrador@PET00LT006647.latam1.prosegur.local>
2024-10-28 21:02:12 -0500

    feat : update connection string

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -51,7 +51,7 @@ var app = builder.Build();
 
 EnsureDatabaseCreation(app);
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
